984e46d69f0f3ae2bf7bc3a874ba70f7e3e0a9ea
Devon Scott <dscott@bluefission.com>
2026-08-07 17:50:26 -0400

    normalize database populate arguments

---
+++ tests/Unit/Business/Console/DatabaseManagerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Unit\Business\Console;
+
+use App\Business\Console\DatabaseManager;
+use BlueFission\Behavioral\Behaviors\Behavior;
+use BlueFission\BlueCore\Business\Managers\DatasourceManager;
+use PHPUnit\Framework\TestCase;
+
+class DatabaseManagerTest extends TestCase
+{
+    public function testPopulateRecognizesAutoAsAPositionalArgument(): void
+    {
+        $datasource = $this->createMock(DatasourceManager::class);
+        $datasource->expects($this->once())
+            ->method('populate')
+            ->with(true);
+
+        $manager = new DatabaseManager($datasource);
+        $behavior = new Behavior('populate');
+        $behavior->context = ['data' => ['auto']];
+
+        $manager->populate($behavior);
+    }
+
+    public function testPopulateRemainsInteractiveWithoutAutoArgument(): void
+    {
+        $datasource = $this->createMock(DatasourceManager::class);
+        $datasource->expects($this->once())
+            ->method('populate')
+            ->with(false);
+
+        $manager = new DatabaseManager($datasource);
+        $behavior = new Behavior('populate');
+        $behavior->context = ['data' => []];
+
+        $manager->populate($behavior);
+    }
+}
